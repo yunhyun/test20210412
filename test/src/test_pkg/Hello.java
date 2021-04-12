a4e7e1ecddc687a9b29715a8a682588b0afcae0f
user <user@DESKTOP-JHIL7O9>
2021-04-12 15:37:44 +0900

    테스트

---
+++ test/src/test_pkg/Hello.java
@@ -0,0 +1,27 @@
+package test_pkg;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 텍스트 또는 변숫값 출력 
+		// print, println 메소드 
+		System.out.println("텍스트 출력할때는 더블쿼터안에");  
+		// 변수 
+		// 어떠한 값을 담기 위한 공간 
+		// 변수 선언방법 : 변수타입 변수이름;, 변수타입 변수이름=값;
+		// 변수 사용방법 : 변수이름 = 값;
+		// 우변의 값을 좌변에 대입한다. 
+		// 코드상에 = 이 있으면 무조건 먼저 좌변을 보고 그다음 우변을 본다. 
+		// 변수 타입 
+		// 정수, 실수, 논리, 문자열 
+		
+		
+	}
+
+}
+
+
+
+
+
